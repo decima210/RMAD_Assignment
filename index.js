db4e765cdcedfd7a8d90edbc74ec91b88405cdcf
Decima <decima567@gmail.com>
2022-10-18 13:53:45 +0530

    SampleJson final

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import SampleJson from './src/SampleJason';
+import SampleJson from './src/SampleJson';
 
 AppRegistry.registerComponent(appName, () => SampleJson);
