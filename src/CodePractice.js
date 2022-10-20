f045419c4ea363746e82babb4d2d482ff12c420e
Decima <decima567@gmail.com>
2022-10-20 16:17:44 +0530

    adding styles

--- src/CodePractice.js
+++ src/CodePractice.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
-import {Text, SafeAreaView} from 'react-native';
+import {Text, SafeAreaView, View, TouchableOpacity} from 'react-native';
+import styles from './CodePracticeStyles';
 
-export default function CodePractice() {
-  return (
-    <SafeAreaView>
-      <Text> Hello World 2</Text>
-    </SafeAreaView>
-  );
+export default class CodePractice extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    };
+  }
+  render() {
+    return (
+      <View style={styles.abc}>
+        <SafeAreaView>
+          <Text> {this.state.counter}</Text>
+          <TouchableOpacity style={styles.btn} />
+        </SafeAreaView>
+      </View>
+    );
+  }
 }
---
+++ src/CodePracticeStyles.js
@@ -0,0 +1,21 @@
+import StyleSheet from 'react';
+
+const styles = StyleSheet.create({
+  abc: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontsize: 20,
+    color: '#227756',
+    padding: 10,
+  },
+  btn: {
+    backgroundColor: '#227756',
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+  },
+});
+export default styles;
