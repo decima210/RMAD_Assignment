3cd60a4bc66f9d96f79c7fd97f2de5b5a23ec933
Decima <decima567@gmail.com>
2022-10-23 09:53:37 +0530

    index change

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import PracticeConditions from './src/PracticeConditions';
+import CodePractice from './src/CodePractice';
 
-AppRegistry.registerComponent(appName, () => PracticeConditions);
+AppRegistry.registerComponent(appName, () => CodePractice);
