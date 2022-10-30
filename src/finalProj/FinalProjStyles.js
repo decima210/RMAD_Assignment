1f64b3eda32642caecee2363c021cc797133fc58
Decima <decima567@gmail.com>
2022-10-31 05:02:19 +0530

    changes

---
+++ src/finalProj/FinalProjStyles.js
@@ -0,0 +1,26 @@
+import {StyleSheet} from 'react-native';
+import styles from './FinalProjStyles';
+
+const styles = StyleSheet.create({
+  abc: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontsize: 20,
+    color: '#227756',
+    padding: 10,
+  },
+  btn: {
+    backgroundColor: '#22ccff',
+    padding: 50,
+    margin: 10,
+    borderRadius: 5,
+  },
+  btnTitle: {
+    fontSize: 26,
+    color: '#000000',
+  },
+});
+export default styles;
